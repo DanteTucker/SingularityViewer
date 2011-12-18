4000fea261a98f4fdacf49831b543aaf1bef2c0c
phr0z3nt04st <cryostasix@gmail.com>
2011-12-18 13:11:52 -0600

    null joint assert removed, optimized v4adapt
    NULL joint assert is unneccessary because there is a null check after that
    method is called. Also v4adapt doesnt need to make a new F32 array because
    you can convert a LLVector4 into a F32 array since it is memory aligned
    and such.

--- indra/newview/aWavefront.h
+++ indra/newview/aWavefront.h
@@ -62,8 +62,11 @@ public:
 class v4adapt
 {
 private:
-	LLVector4a* v4a;
+	LLStrider<LLVector4a> mV4aStrider;
 public:
-	v4adapt(LLVector4a* vp) { v4a = vp; }
-	LLVector3& operator[] (const unsigned int i);
+	v4adapt(LLVector4a* vp);
+	inline LLVector3 v4adapt::operator[] (const unsigned int i)
+	{
+		return LLVector3((F32*)&mV4aStrider[i]);
+	}
 };
